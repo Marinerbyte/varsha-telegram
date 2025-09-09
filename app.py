c376043651d97384d3367f6d2cc8528ea980f886
Marinerbyte <yasinkarani14@gmail.com>
2025-09-09 23:47:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ if __name__ == "__main__":
     else:
         logger.info(f"Bot '{BOT_USERNAME}' chal raha hai...")
         port = int(os.environ.get("PORT", 8080))
-        app.run(host='0.0.0.0', port=port)```
+        app.run(host='0.0.0.0', port=port)
